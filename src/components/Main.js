67d208d673569887e6ac39969818e53bad3b82ee
StefanyCandida <stefany.candida@gmail.com>
2022-10-24 17:31:26 -0300

    alteração de styled

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,11 +12,11 @@ export default function Main() {
         <>
             <S.GlobalStyle />
             <S.Main>
-                <div>
+                <S.DivMain>
                     <h3>LATEST RECIPES</h3>
-                    <span></span>
-                </div>
-                <section>
+                    <S.Span></S.Span>
+                </S.DivMain>
+                <S.Section>
                     <S.FigureBox>
                         <S.Images src={bolo} alt="" />
                         <span></span>
@@ -30,10 +30,10 @@ export default function Main() {
                     <S.FigureBox>
                         <S.Images src={vitamina} alt="" />
                         <span></span>
-                        <figcaption>Peanut Smoothie</figcaption>
+                        <S.Figcaption>Peanut Smoothie</S.Figcaption>
                     </S.FigureBox>
-                </section>
-                {/* <div className="about">
+                </S.Section>
+              <S.Section>
                     <figure>
                     <S.Images src={colher} alt="" />
                     </figure>
@@ -43,7 +43,7 @@ export default function Main() {
                         <p>Quisque ut tellus urna. Phasellus ipsum sapien, porttitor ut feugiat quis, faucibus id arcu. Cras ultrices erat ac mi facilisis tristique. In finibus nisi mauris, ornare venenatis augue condimentum lobortis. Nullam nec sapien sit amet odio ultricies accumsan ut vitae eros. Fusce pellentesque sed justo nec sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum eget ipsum eget mi vehicula sollicitudin eu eu ipsum. Duis varius neque purus, gravida viverra nulla porttitor ac. Fusce eleifend eu velit ac mattis. Fusce eget lectus tortor. Ut vel nibh et tortor maximus laoreet.
                             Integer ante sapien, porttitor in aliquet sit amet, congue sed augue. Nunc a tincidunt sapien. In at tempus nisl. Sed ultricies at nibh sed elementum. Integer convallis urna magna, quis eleifend ligula pulvinar vel.</p>
                     </div>
-                </div> */}
+                </S.Section> 
             </S.Main>
         </>)
 }
